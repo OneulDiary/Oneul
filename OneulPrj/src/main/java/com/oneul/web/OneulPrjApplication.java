00a5314b7e104f5ac5b1b952a65c493b1344d6c5
hmountainn <ghkdwltks123@naver.com>
2021-08-15 19:47:38 +0900

    war로 배포 설정

--- OneulPrj/src/main/java/com/oneul/web/OneulPrjApplication.java
+++ OneulPrj/src/main/java/com/oneul/web/OneulPrjApplication.java
@@ -2,10 +2,17 @@ package com.oneul.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OneulPrjApplication {
+public class OneulPrjApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OneulPrjApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OneulPrjApplication.class, args);
 	}
